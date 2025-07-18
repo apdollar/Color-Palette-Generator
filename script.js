b57ac1c3dffa21727896a84ccae424eb8c3e286b
apdollar <iamaadiprj@gmail.com>
2025-07-18 13:24:29 +0545

    all new changes js

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function getRandomColor() {
   return "#" + color.padStart(6, "0");
 }
 
+function clearFeedback() {
+  setTimeout(() => {
+    feedback.innerText = "";
+  }, 1500);
+}
+
+function generatePalette(numColors=5) {
+   paletteDiv.innerHTML = ''; // clear previous palette
+  feedback.innerText = '';
+
+  for (let i = 0; i < numColors; i++) {
+    const color = getRandomColor();
+
+     const colorBox = document.createElement('div');
+     colorBox.style.backgroundColor = color;
+      colorBox.style.width = '100px';
+    colorBox.style.height = '100px';
+    colorBox.style.margin = '10px';
+    colorBox.style.borderRadius = '10px';
+    colorBox.style.display = 'flex';
+    colorBox.style.justifyContent = 'center';
+    colorBox.style.alignItems = 'center';
+    colorBox.style.cursor = 'pointer';
+    colorBox.style.color = 'white';
+    colorBox.style.fontWeight = 'bold';
+    colorBox.style.userSelect = 'none';
+    colorBox.textContent = color;
